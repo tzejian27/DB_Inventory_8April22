a7183ea65d71ce5e6b1be7cd02176d639a97c930
tzejian27 <82267834+tzejian27@users.noreply.github.com>
2022-04-15 11:03:05 +0800

    access right

--- app/src/main/java/com/example/db_inventory/Access_Right.java
+++ app/src/main/java/com/example/db_inventory/Access_Right.java
@@ -10,5 +10,7 @@ public class Access_Right extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_access_right);
+
+        setTitle("Access Right");
     }
 }
