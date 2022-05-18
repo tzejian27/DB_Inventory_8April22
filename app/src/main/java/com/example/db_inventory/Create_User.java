522a673f2fc04efd6514e340ad635f76cf6692de
barcodingApril <barcodingtay@gmail.com>
2022-05-18 14:33:51 +0800

    replace slash input .replace("/", "|")

--- app/src/main/java/com/example/db_inventory/Create_User.java
+++ app/src/main/java/com/example/db_inventory/Create_User.java
@@ -62,7 +62,7 @@ public class Create_User extends AppCompatActivity {
 
         //create user
         btn_create.setOnClickListener(view -> {
-            //use replace to avoid using of "/" symbol
+            //replace "/" by symbol "|"
             String username = et_username.getText().toString().trim().replace("/", "|");
             String pass1 = et_pass1.getText().toString().trim();
             String pass2 = et_pass2.getText().toString().trim();
